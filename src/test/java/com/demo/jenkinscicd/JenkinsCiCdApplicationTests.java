bc08b3255253a30dc956bde4d5d12c72ceeb1485
HARDEV RAJPUT <rajput.hardev@gmail.com>
2024-03-03 00:58:46 +0530

    first commit

---
+++ src/test/java/com/demo/jenkinscicd/JenkinsCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.jenkinscicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
